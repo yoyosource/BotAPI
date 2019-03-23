c71fabea7c8ca80506f9c2077af62f7cbe2ea426
jojo <yoyosource@nidido.de>
2019-03-23 22:11:59 +0100

    Merge branch 'master' of /Users/jojo/IdeaProjects/BotAPI with conflicts.

--- src/main/java/botapi/bot/Bot.java
+++ src/main/java/botapi/bot/Bot.java
@@ -2,4 +2,15 @@ package botapi.bot;
 
 public class Bot {
 
+    JDABuilder jda;
+
+    public Bot(String token) {
+        jda = new JDABuilder(AccountType.BOT);
+        jda.setToken(token);
+    }
+
+    public void setGame() {
+
+    }
+
 }
