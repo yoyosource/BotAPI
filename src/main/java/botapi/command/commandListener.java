5c85b909beec5106a57d1fe574a65937051174c8
maxsp <max.spaeth01@gmail.com>
2019-03-23 22:13:08 +0100

    Test

--- src/main/java/botapi/command/commandListener.java
+++ src/main/java/botapi/command/commandListener.java
@@ -1,4 +1,14 @@
 package botapi.command;
 
-public class commandListener {
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.core.hooks.ListenerAdapter;
+
+public class commandListener extends ListenerAdapter {
+    public void onMessageReceived(MessageReceivedEvent event) {
+
+        if (event.getMessage().getContentRaw().startsWith("-") && event.getMessage().getAuthor().getId() != event.getJDA().getSelfUser().getId()) {
+            commandHandler.handleCommand(commandHandler.parse.parse(event.getMessage().getContentRaw(), event));
+        }
+
+    }
 }
